4409186a1992ac235714c62b026654e8266f5611
Vamsee431 <vamsee431@gmail.com>
2014-07-23 01:04:29 +0530

    Added Home Page
    Dialog Boxes Are Done for Logins And Contact Us

---
+++ MyTrack/MyTrack/Scripts/MyScript.js
@@ -0,0 +1,106 @@
+﻿$(document).ready(function () {
+    $("#admin").dialog({
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        modal: true,
+        my: "center",
+        at: "center",
+        of: window,
+        width: 400,
+        dialogClass: 'no-close',
+        buttons: {
+            "Close": CloseDialog,
+            "Clear": ClearAll
+        }
+    })
+    function CloseDialog() {
+        $("#admin").dialog("close");
+    }
+
+    function ClearAll() {
+        $('#txtEmail').val("");
+        $('#txtPassword').val("");
+    }
+    $("#btnadmin").click(function () {
+        $("#admin").dialog("open");
+        return false;
+    })
+
+    $("#Passenger").dialog({
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        modal: true,
+        my: "center",
+        at: "center",
+        of: window,
+        width: 400,
+        dialogClass: 'no-close',
+        buttons: {
+            "Close": CloseDialog,
+            "Clear": ClearAll
+        }
+    })
+    function CloseDialog() {
+        $("#Passenger").dialog("close");
+    }
+
+    function ClearAll() {
+        $('#txtEmail').val("");
+        $('#txtPassword').val("");
+    }
+    $("#btnPassenger").click(function () {
+        $("#Passenger").dialog("open");
+        return false;
+    })
+
+    //$("#Registration").dialog({
+    //    autoOpen: false,
+    //    draggable: false,
+    //    resizable: false,
+    //    modal: true,
+    //    my: "center",
+    //    at: "center",
+    //    of: window,
+    //    width: 400,
+    //    dialogClass: 'no-close',
+    //    buttons: {
+    //        "Close": CloseDialog,
+    //        "Clear": ClearAll
+    //    }
+    //})
+    //function CloseDialog() {
+    //    $("#Registration").dialog("close");
+    //}
+    //function ClearAll() {
+    //    $('#txtEmail').val("");
+    //    $('#txtPassword').val("");
+    //}
+    //$("#btnPassenger").click(function () {
+    //    $("#Passenger").dialog("open");
+    //    return false;
+    //})
+    $("#Contact").dialog({
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        modal: true,
+        my: "center",
+        at: "center",
+        of: window,
+        width: 400,
+        dialogClass: 'no-close',
+        buttons: {
+            "Close": CloseDialog
+        }
+    })
+    function CloseDialog() {
+        $("#Contact").dialog("close");
+    }
+    $("#btnContact").click(function () {
+        $("#Contact").dialog("open");
+        return false;
+    })
+    });
+    
