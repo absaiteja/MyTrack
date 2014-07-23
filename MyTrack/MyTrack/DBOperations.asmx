6be35ddecd6c312dbb71712ce0bf82d012b27ed1
dvani <vani409.dasari@gmail.com>
2014-07-23 12:23:40 +0530

    Test

--- MyTrack/MyTrack/DBOperations.asmx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebService Language="C#" CodeBehind="DBOperations.asmx.cs" Class="MyTrack.DBOperations" %>
--- MyTrack/MyTrack/Scripts/Admin.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $('#tabs').tabs();
-    
-    $(".chosen").chosen({ no_results_text: "Oops, nothing found!" });
-    $(".chosen").chosen({ max_selected_options: 5 });
-});
